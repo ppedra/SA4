f54c07bf0f1a94ca5f4a42c18f22345e3b43b4fe
ppedra <andrehagemann@hotmail.com>
2018-10-22 21:13:03 -0300

    Camera follows player and look at the sea when near

---
+++ Assets/Scripts/CameraMoviment.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoviment : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject pointToFollow;
+
+	[Header("Values for moving")]
+	public float amount;
+	public float whereItStarts;
+	public float maxNegY;
+
+	void Start () {
+		if (player == null || pointToFollow == null){
+			Debug.LogError("missing ref in CameraMoviment!");
+		}
+		whereItStarts = -1f;
+		amount = 2f;
+	}
+	
+	void Update () {
+		MovePointToFollow();
+	}
+
+	///<summary>
+	///if player.position.y reaches a point (whereItStarts), pointToFollow will move lower, so the
+	///camera can look at the ocean
+	///</summary>
+	private void MovePointToFollow(){
+		if (player.transform.position.y < whereItStarts){
+			float tempy = Mathf.Clamp((player.transform.position.y - whereItStarts)* amount, maxNegY,0f);
+			Vector3 tempPos = new Vector3(0f, tempy, 0f);
+			pointToFollow.transform.localPosition = tempPos;
+		}else{
+			pointToFollow.transform.localPosition = Vector3.zero;
+		}
+	}
+}
